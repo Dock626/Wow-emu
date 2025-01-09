5753d991aba7f8fb079de2d6c93de88077a0ee3e
Dock626 <oskarosmolak@gmail.com>
2025-01-09 16:46:58 +0100

    feature: Added mouse tracking by raycasting

---
+++ Spells/aoe.gd
@@ -0,0 +1,14 @@
+extends Area3D
+#@export var spawnPos : Vector3
+#var space_state := get_world_3d().direct_space_state
+#var query := PhysicsRayQueryParameters3D.create(origin, end)
+#var result := space_state.intersect_ray(query)
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#position = spawnPos
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
