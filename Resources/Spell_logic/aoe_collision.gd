e44cfb3772d14521655dea44a586f7ba4095f81c
Dock626 <oskarosmolak@gmail.com>
2025-02-03 14:25:45 +0100

    fix: small fix on aoe

--- Resources/Spell_logic/aoe_collision.gd
+++ Resources/Spell_logic/aoe_collision.gd
@@ -11,7 +11,8 @@ var bodies
 var actions : Array[BaseSpellAction] = []
 var frame = 0
 var initial_use :bool = false
-
+var affect_players = false
+var affect_mobs = true
 func _ready() -> void:
 	if effect_time > 0:
 		timer.wait_time = effect_time
@@ -26,21 +27,21 @@ func _physics_process(delta: float) -> void:
 			
 func use_once():
 	bodies = get_overlapping_bodies()
-	for body in bodies:
-		if body.is_in_group("Mobs") or body.is_in_group("Players"):
-			self.actions_emit.connect(body._on_actions_received)
-			actions_emit.emit(actions)
-			self.actions_emit.disconnect(body._on_actions_received)
+	emit_actions()
 	if effect_time == 0:
 		queue_free()
 	initial_use = true
 
 func _on_actions_emit_timeout() -> void:
-	for body in bodies:
-		if body.is_in_group("Mobs") or body.is_in_group("Players"):
-			self.actions_emit.connect(body._on_actions_received)
-			actions_emit.emit(actions)
-			self.actions_emit.disconnect(body._on_actions_received)
+	emit_actions()
+
+func emit_actions():
+	if affect_players and affect_mobs:
+		for body in bodies:
+			if (affect_players and body.is_in_group("Players")) or (affect_mobs and body.is_in_group("Mobs")):
+				self.actions_emit.connect(body._on_actions_received)
+				actions_emit.emit(actions)
+				self.actions_emit.disconnect(body._on_actions_received)
 
 func _on_timer_timeout() -> void:
 	self.queue_free()
