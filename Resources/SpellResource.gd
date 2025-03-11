41df3bd6f323ce8894c7fcf9f3838d1cb3fdc8eb
Dock626 <oskarosmolak@gmail.com>
2025-03-11 11:45:33 +0100

    fix: deleted init of spellresources, simplified

--- Resources/SpellResource.gd
+++ Resources/SpellResource.gd
@@ -17,39 +17,23 @@ enum cast_type {
 var caster: Node
 var actions: Array[BaseSpellAction] = []
 var procs: Array[proc_buff] = []
-var name: String
-var description: String
-var type: cast_type
-var energy_cost: int
+var name: String = ""
+var description: String = ""
+var type: cast_type = cast_type.Instant
+var energy_cost: int = 0
 var cast_time: float = 0
 var is_GCD: bool = true
 var icon
 var default_icon = preload("res://Resources/icons/fajerbol.png")
 var proc_check: String
 var cast_only_while_standing: bool = true
-var CD : float
-#AoE properties
+var CD: float
+# AoE properties
 var cast_position: Vector3
-var cast_radius: float
+var cast_radius: float = 0
 var effect_time: float
 var tick_rate: float
 
-func _init(name: String = "", description: String = "", type: cast_type = 
-			cast_type.Instant, energy: int = 0, cast_time: float = 0,
-			is_GCD: bool = true, icon = self.default_icon,
-			actions: Array[BaseSpellAction] = [], procs: Array[proc_buff] = [], 
-			aoe_radius: float = 0) -> void:
-	self.actions.append_array(actions)
-	self.procs.append_array(procs)
-	self.name = name
-	self.description = description
-	self.type = type
-	self.energy_cost = energy
-	self.cast_time = cast_time
-	self.is_GCD = is_GCD
-	self.icon = icon
-	self.cast_radius = aoe_radius
-	
 func add_action(action):
 	self.actions.append(action)
 
@@ -60,11 +44,20 @@ func is_procced(user, button):
 	for buff in user.buffs:
 		if buff is proc_buff and button.Spell.proc_check == buff.proc_name:
 			return true
+
 func duplicate_spell() -> SpellResource:
 	# Create a new instance of SpellResource
-	var new_spell = SpellResource.new(self.name, self.description, self.type, self.energy_cost, self.cast_time, self.is_GCD, self.icon, [], [], self.cast_radius)
+	var new_spell = SpellResource.new()
 
 	# Copy primitive values
+	new_spell.name = self.name
+	new_spell.description = self.description
+	new_spell.type = self.type
+	new_spell.energy_cost = self.energy_cost
+	new_spell.cast_time = self.cast_time
+	new_spell.is_GCD = self.is_GCD
+	new_spell.icon = self.icon
+	new_spell.cast_radius = self.cast_radius
 	new_spell.spellClass = self.spellClass
 	new_spell.spellIcon = self.spellIcon
 	new_spell.cast_position = self.cast_position
@@ -73,10 +66,16 @@ func duplicate_spell() -> SpellResource:
 	new_spell.proc_check = self.proc_check
 	new_spell.cast_only_while_standing = self.cast_only_while_standing
 	new_spell.CD = self.CD
-	# Manually copy actions (ensure they support duplication)
+
+	# Duplicate actions
 	for action in self.actions:
-		action.spell = new_spell
-		action.name =new_spell.name
-		new_spell.actions.append(action.duplicate_action())
+		var duplicated_action = action.duplicate_action()
+		duplicated_action.spell = new_spell
+		duplicated_action.name = new_spell.name
+		new_spell.actions.append(duplicated_action)
+
+	# Duplicate procs
+	for proc in self.procs:
+		new_spell.procs.append(proc.duplicate())
 
 	return new_spell
